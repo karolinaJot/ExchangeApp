d3b2eb37afdd3c35ee890898d709c8914dd38943
karolinaJot <amicus.karo@gmail.com>
2020-11-04 17:14:06 +0100

    Finish form, add view which confirm adding new item.

--- ExchangeApp.Web/Controllers/ExchangesController.cs
+++ ExchangeApp.Web/Controllers/ExchangesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExchangeApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,24 @@ namespace ExchangeApp.Web.Controllers
 {
 	public class ExchangesController : Controller
 	{
+		[HttpGet]
 		public IActionResult Show()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Show(ItemModel item)
+		{
+			var viewModel = new ItemAddedModel
+			{
+				NumberOfCharsInName = item.Name.Length,
+				NumberofCharsInDescription = item.Description.Length,
+				IsHidden = !item.IsVisible
+			};
+
+			return View("ItemAdded", viewModel);
+			//return RedirectToAction("ItemAdded");
 		}
 	}
 }
