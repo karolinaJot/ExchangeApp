ee76a71b3d963597317fa9043ed5c416aa7f5177
karolinaJot <amicus.karo@gmail.com>
2020-11-24 10:46:47 +0100

    Lab 3: Add database to the project. Error - problem with connection to SQL Server  ---To Fix---

---
+++ ExchangeApp.Web/Database/ExchangesDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ExchangeApp.Web.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExchangeApp.Web.Database
+{
+	public class ExchangesDbContext : DbContext
+	{
+		public ExchangesDbContext(DbContextOptions options)
+			: base(options)
+		{ 
+		}
+
+		public DbSet<ItemEntity> Item { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{ 
+		
+		}
+
+
+
+	}
+}
---
+++ ExchangeApp.Web/Migrations/20201124093833_AddItemToDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExchangeApp.Web.Migrations
+{
+    public partial class AddItemToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    IsVisible = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Item");
+        }
+    }
+}
